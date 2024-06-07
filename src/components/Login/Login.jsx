0030adbea22fff242143c9ae9830634589e47e0a
kashif2666 <kashifjsr7@gmail.com>
2024-06-08 00:58:49 +0530

    Admin Page Added

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import "./Login.css";
+import { Button, Typography } from "@mui/material";
+
+const Login = () => {
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div className="login">
+      <div className="loginContainer">
+        <form className="loginForm" onSubmit={submitHandler}>
+          <Typography variant="h4">
+            <p>A</p>
+            <p>D</p>
+            <p>M</p>
+            <p>I</p>
+            <p style={{ marginRight: "1vmax" }}>N</p>
+
+            <p>P</p>
+            <p>A</p>
+            <p>N</p>
+            <p>E</p>
+            <p>L</p>
+          </Typography>
+          <div>
+            <input
+              type="email"
+              placeholder="Admin Email"
+              value={email}
+              required
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+            <input
+              type="password"
+              placeholder="Admin Password"
+              value={password}
+              required
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+            <Button variant="contained" type="submit">
+              Login
+            </Button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
